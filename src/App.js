21150d0df9198b55657515a83fea4c8cb2a28d20
Nur Kasimov <nur.kas99@ya.ru>
2021-04-08 17:07:06 +0500

    map

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Map from "./components/Map/Map";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Map url="Парк Вайнера.geojson"/>
+      </main>
+    </>
+  );
+}
+
+export default App;

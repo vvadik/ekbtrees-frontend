f37b37fd3c1beba20fbdcef4bc8b50e45971c88a
Краснояров Сергей Алексеевич <skrasnoyarov@naumen.ru>
2021-05-30 21:12:48 +0500

    Правка адаптивной вертски на форме добавления дерева

--- src/components/AddNewTreeForm/AddNewTreeForm.js
+++ src/components/AddNewTreeForm/AddNewTreeForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styles from "./AddNewTreeForm.css";
+import "./AddNewTreeForm.css";
 
 export default class AddNewTreeForm extends Component {
     render() {
@@ -10,15 +10,15 @@ export default class AddNewTreeForm extends Component {
                 <h3 class="center h3"> Карточка дерева </h3>
                 <div class="center p"> пожалуйста, добавьте  всю информацию о дереве  </div>
                 <h4 class="center h4"> Основная информация</h4>
-                <form>
+                <form className="addTreeForm">
                     <figure class="main">
                         <label>
-                            <span> Геопозиция </span>
+                            <span className="addTreeFormMainTitle"> Геопозиция </span>
                             <input type="text" maxlength="10" disabled value = {`${lat}, ${lng}`}/>
                         </label>
                         <div>
                             <label>
-                                <span> Порода </span>
+                                <span className="addTreeFormMainTitle"> Порода </span>
                                 <select dir="rtl">
                                     <option> Дуб </option>
                                     <option> Клён </option>
@@ -28,62 +28,58 @@ export default class AddNewTreeForm extends Component {
                         </div>
                         <div>
                             <label class="block">
-                                <span> Высота в  метрах </span>
+                                <span className="addTreeFormMainTitle"> Высота (в метрах)</span>
                                 <label>
                                     <input type="number" min="1" max="50"/>
                                 </label>
-                                <label class="metric margin"> М </label>
                             </label>
-                            <label class="block">
-                                <span> Диаметр  кроны </span>
+                            <label className="block">
+                                <span className="addTreeFormMainTitle">Диаметр кроны (в метрах)</span>
                                 <label>
                                     <input type="number" min="1" max="50"/>
                                 </label>
-                                <label class="metric margin"> М </label>
                             </label>
                         </div>
                         <div>
                             <label class="block">
-                                <span> Обхват <wbr/> (самого  толстого)   ствола </span>
+                                <span className="addTreeFormMainTitle"> Обхват <wbr/> самого толстого ствола (в сантиметрах)</span>
                                 <label>
                                     <input required type="number" min="1" max="10"/>
                                 </label>
-                                <label class="metric margincm"> СМ </label>
                             </label>
-                            <label class="block">
-                                <span> Число стволов </span>
+                            <label className="block">
+                                <span className="addTreeFormMainTitle"> Число стволов </span>
                                 <label>
                                     <input required type="number" min="1" max="5" placeholder="1"/>
                                 </label>
                             </label>
                         </div>
                         <div>
-                            <label class="block">
-                                <span> Высота первой  ветви от земли  в метрах  </span>
+                            <label className="block">
+                                <span className="addTreeFormMainTitle"> Высота первой ветви от земли (в метрах)</span>
                                 <label>
                                     <input required type="number" min="1" max="50"/>
                                 </label>
-                                <label class="metric margin"> М </label>
                             </label>
-                            <label class="block">
-                                <span> Возраст в годах </span>
+                            <label className="block">
+                                <span className="addTreeFormMainTitle"> Возраст (в годах)</span>
                                 <input required type="number" min="0" max="100"/>
                             </label>
                         </div>
                         <div>
-                            <span> Визуальная  оценка   состояния </span>
+                            <span className="addTreeFormMainTitle">Визуальная  оценка состояния</span>
                             <label>
                                 <select dir="rtl">
-                                    <option> 1/5</option>
-                                    <option> 2/5 </option>
-                                    <option> 3/5 </option>
-                                    <option> 4/5 </option>
-                                    <option> 5/5 </option>
+                                    <option>1/5</option>
+                                    <option>2/5</option>
+                                    <option>3/5</option>
+                                    <option>4/5</option>
+                                    <option>5/5</option>
                                 </select>
                             </label>
                         </div>
                         <div>
-                            <span> Культурная  посадка   или самосев </span>
+                            <span className="addTreeFormMainTitle">Культурная  посадка или самосев</span>
                             <label>
                                 <select dir="rtl">
                                     <option> Культурная посадка </option>
@@ -92,32 +88,32 @@ export default class AddNewTreeForm extends Component {
                             </label>
                         </div>
                     </figure>
-                    <h4 class="center h4"> фотографии и файлы </h4>
-                    <figure class="main">
+                    <h4 className="center h4"> фотографии и файлы </h4>
+                    <figure className="main">
                         <div>
-                        <form class="file">
-                                <figure class="picture"> </figure>
-                                <input type="file"  multiplie name="upload" class="fileform upload" />
-                                <div class="fileform">
-                                    <img src="/upload.png" class="center"/>
-                                    <label class="fileformlabel"  id="fileformlabel"></label>
+                        <form className="file">
+                                <figure className="picture"> </figure>
+                                <input type="file"  multiplie name="upload" className="fileform upload" />
+                                <div className="fileform">
+                                    <img src="/upload.png" className="center"/>
+                                    <label className="fileformlabel"  id="fileformlabel" />
                                     <label class="selectbutton">Browse..</label>
                                 </div>
                         </form>
                     </div>
                     </figure>
-                    <h4 class="center h4"> Дополнительная информация</h4>
-                    <figure class="main">
+                    <h4 className="center h4"> Дополнительная информация</h4>
+                    <figure className="main">
                         <div>
-                            <label class="block">
-                                <span> Автоназначаемый  идентификатор </span>
+                            <label className="block">
+                                <span className="addTreeFormMainTitle"> Автоназначаемый  идентификатор </span>
                                 <label>
-                                    <span class="identifier"> 10321 </span>
+                                    <span className="identifier"> 10321 </span>
                                 </label>
                             </label>
-                            <label class="block">
-                                <span> Статус дерева </span>
-                                <select dir="rtl" class="status">
+                            <label className="block">
+                                <span className="addTreeFormMainTitle"> Статус дерева </span>
+                                <select dir="rtl" className="status">
                                     <option> Жив </option>
                                     <option> Цел </option>
                                     <option> Орел </option>
@@ -125,27 +121,27 @@ export default class AddNewTreeForm extends Component {
                             </label>
                         </div>
                         <div>
-                            <label class="block">
-                                <span> Дата и время  добавления  записи  </span>
+                            <label className="block">
+                                <span className="addTreeFormMainTitle"> Дата и время  добавления  записи  </span>
                                 <label>
                                     <input type="datetime-local"/>
                                 </label>
                             </label>
-                            <label class="block">
-                                <span> Дата и время  последнего  редактирования  </span>
+                            <label className="block">
+                                <span className="addTreeFormMainTitle"> Дата и время  последнего  редактирования  </span>
                                 <label>
                                     <input type="datetime-local"/>
                                 </label>
                             </label>
                         </div>
                         <div>
-                            <span> Ссылка на  автора </span>
+                            <span className="addTreeFormMainTitle"> Ссылка на  автора </span>
                             <label>
-                                <input type="email" name="login" autocorrect="off" maxlength="60"/>  
+                                <input type="email" name="login" autoCorrect="off" maxLength="60"/>
                             </label>
                         </div>
                     </figure>
-                    <div class="submit">
+                    <div className="submit">
                         <input type="submit" value="Отправить"/>
                     </div>
                 </form>

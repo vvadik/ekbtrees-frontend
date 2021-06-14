136d6b8e2e70e9cece45792bc398054e0c675dce
Antonov Alexey <aleksei193@mail.ru>
2021-06-15 00:58:12 +0500

    Исправлена главная страница (мобильная версия)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,55 +6,36 @@ import styles from './Home.module.css';
 import './Map.css';
 
 export default class Home extends Component {
-  renderMobileSection() {
-    return (
-      <section className={styles.mobile}>
-        <div className={styles.mapContainer}>
-          <div className={styles.map}>
-            <MapContain />
-          </div>
-        </div>
-      </section>
-    )
-  }
   renderDesktopSection() {
     return (
       <section className={styles.desktop}>
         <div className={styles.flexSearchMap}>
           <div className={styles.search}>
             <div className={cn([styles.green, styles.searchBlock])} />
-            <div>
-              <div className={styles.containerSearch}>
-                <span className={styles.icon}><i className="fa fa-search" /></span>
-                <input type="search" id="search" className={styles.inputSearch} placeholder="Искать..." />
-              </div>
-            </div>
             <p className={styles.slogan}>Защитим деревья вместе</p>
-            <p className={styles.regular}>
-              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
-              used in laying out print, graphic or web designs.
-                </p>
+            <p className={styles.regular2}>
+              Нанесите любимые деревья на карту, чтобы документировать их количество и реальное состояние. Объединяйтесь с волонтёрами из вашего района для совместной защиты зелёных насаждений, используя карту, как единый источник информации.
+            </p>
           </div>
           <div className={styles.desktopMap}>
             <div className={cn([styles.green, styles.mapBlock])} />
-            <div>
               <div className={styles.mapContainer}>
                 <div className={styles.map}>
                   <MapContain/>
                 </div>
-              </div>
             </div>
           </div>
         </div>
       </section>
     )
   }
+
   renderGetStartSection() {
     return (
       <section className={cn([styles.green, styles.getStart, styles.desktop])}>
         <div className={styles.getStartLeft}>
           <p className={cn([styles.regular, styles.bold])}>Готовы начать?</p>
-          <p className={styles.regular}>Войдите или зарегистрируйтесь</p>
+          <p className={cn([styles.regular, styles.inOrReg])}>Войдите или зарегистрируйтесь</p>
         </div>
         <div className={styles.getStartRight}>
           <NavLink className={styles.logInDesktop} exact to='/login' activeclassname="active">Войти</NavLink>
@@ -63,20 +44,25 @@ export default class Home extends Component {
       </section>
     )
   }
+
   renderLastPartSection() {
     return (
       <section className={cn([styles.desktop, styles.lastPart])}>
         <div className={styles.flexContainer}>
           <div className={styles.leftPart}>
-            <h3 className={cn([styles.aboutTrees, styles.regular, styles.bold])}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h3>
-            <div className={styles.flexContainer}>
+            <h3 className={cn([styles.aboutTrees, styles.regular, styles.bold])}>
+              Ежегодно Екатеринбург теряет сотни взрослых деревьев. Пора&nbsp;действовать!
+            </h3>
+            <div className={styles.flexContainer2}>
               <div className={styles.leftPart}>
-                <i className={cn([styles.fas, "fa-box-open"])} />
-                <p className={styles.regularSmall}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, officia ea delectus distinctio neque et eius accusantium ipsa enim eligendi?</p>
+                <p className={styles.regularSmall}>
+                  Городские деревья часто страдают при строительстве. Лишь немногие застройщики осознают ценность уличного озеленения и высаживают крупномеры вдоль фасадов. Большинство же компаний экономит на озеленении, в результате чего качество среды снижается. С помощью этой карты волонтёры смогут требовать сохранения или, как минимум, эквивалентного восстановления зелёной массы уже на стадии общественного обсуждения проекта.
+                </p>
               </div>
               <div className={styles.rightPart}>
-                <i className={cn([styles.fas, "fa-truck"])} />
-                <p className={styles.regularSmall}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, officia ea delectus distinctio neque et eius accusantium ipsa enim eligendi?</p>
+                <p className={styles.regularSmall}>
+                Ежегодно в результате жестокой обрезки и некомпетентого ухода в городе страдают тысячи деревьев. Многие из них долго болеют и умирают. Из-за отсутствия комплексного подхода, никто не занимается их восстановлением. Карта деревьев даст волонтёрам фактологическую основу для контроля нерадивых исполнителей. Ответственные подрядчики, напротив, снизят потери при помощи актуальных данных.
+                </p>
               </div>
             </div>
           </div>
@@ -87,10 +73,10 @@ export default class Home extends Component {
       </section>
     )
   }
+
   render() {
     return (
       <div>
-        {this.renderMobileSection()}
         {this.renderDesktopSection()}
         {this.renderGetStartSection()}
         {this.renderLastPartSection()}

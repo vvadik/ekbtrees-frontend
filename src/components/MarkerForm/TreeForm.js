05d2206c565aecc699bbfd21d12c199c8de732c3
Краснояров Сергей Алексеевич <skrasnoyarov@naumen.ru>
2021-06-19 15:32:49 +0500

    Фикс модалки на карте

--- src/components/MarkerForm/TreeForm.js
+++ src/components/MarkerForm/TreeForm.js
@@ -6,6 +6,7 @@ import {NavLink} from "react-router-dom";
 export const TreeForm = ({activeTree, onClose}) => {
     return (
         <figure className={styles.block}>
+            <div className={styles.closeWrapper}><button className={styles.close}><i className="fa fa-times" /></button></div>
             <h3 className={styles.title}> Карточка дерева </h3>
             <div className={styles.wrapperFlex}>
                 <div className={cn([styles.blockWrapper, styles.blockWrapperDesktop])}>
@@ -24,27 +25,9 @@ export const TreeForm = ({activeTree, onClose}) => {
                     <span className={styles.blockPrefix}>Диаметр кроны (в метрах)</span>
                     <input className={styles.blockValue} type="text" disabled value = {activeTree?.diameterOfCrown} />
                 </div>
-                {/*<div className={cn([styles.blockWrapper, styles.blockWrapperDesktop])}>*/}
-                {/*    <span*/}
-                {/*        className={styles.blockPrefix}> Обхват <wbr/> самого толстого ствола (в сантиметрах)</span>*/}
-                {/*    <input className={styles.blockValue} disabled type="text" value = {activeTree.trunkGirth} />*/}
-                {/*</div>*/}
-                {/*<div className={cn([styles.blockWrapper, styles.blockWrapperDesktop])}>*/}
-                {/*    <span className={styles.blockPrefix}> Число стволов </span>*/}
-                {/*    <input className={styles.blockValue} type="text" disabled value = {activeTree.numberOfTreeTrunks}/>*/}
-                {/*</div>*/}
-                {/*<div className={cn([styles.blockWrapper, styles.blockWrapperDesktop])}>*/}
-                {/*    <span className={styles.blockPrefix}> Высота первой ветви от земли (в метрах)</span>*/}
-                {/*    <input className={styles.blockValue} type="text" disabled value = {activeTree.heightOfTheFirstBranch}/>*/}
-                {/*</div>*/}
-                {/*<div className={cn([styles.blockWrapper, styles.blockWrapperDesktop])}>*/}
-                {/*    <span className={styles.blockPrefix}> Возраст (в годах)</span>*/}
-                {/*    <input className={styles.blockValue} type="text" min="0" disabled value = {activeTree.age}/>*/}
-                {/*</div>*/}
             </div>
             <div className={styles.navigation}>
                 <NavLink to={`trees/tree=${activeTree?.id}`} className={styles.link} >Узнать подробнее</NavLink>
-                <button className={styles.close} onClick={onClose}>Зыкрыть</button>
             </div>
         </figure>
     )

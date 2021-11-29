84ec40b1c8a0847a7d67938ac2dd0e80b1698654
Vadim Verein <vadikverein45@gmail.com>
2021-11-29 11:00:44 +0500

    Сделал синхронный запрос токена

--- src/helpers/requests.ts
+++ src/helpers/requests.ts
@@ -5,48 +5,51 @@ const cookies = new Cookies();
 
 export default class RequestService {
 	static getData (url: string, headers: HeadersInit = {}) {
-		this.refreshToken();
-		const token = cookies.get('AccessToken');
-		return fetch(url, {
-			method: 'GET',
-			headers: {
-				...headers,
-				'Authorization': `Bearer ${token}`
-			},
-		})
-			.then(response => {
-				if (response.status !== 200) {
-					throw `${response.status} ${response.statusText}`;
-				}
+		this.refreshToken().then(() => {
+			const token = cookies.get('AccessToken');
 
-				return response.json();
-			});
+			return fetch(url, {
+				method: 'GET',
+				headers: {
+					...headers,
+					'Authorization': `Bearer ${token}`
+				},
+			})
+				.then(response => {
+					if (response.status !== 200) {
+						throw `${response.status} ${response.statusText}`;
+					}
+
+					return response.json();
+				});
+		})
 	}
 
 	// TODO: find а more specific type for body
 	static postData (url: string, body: BodyInit | null | undefined, headers: HeadersInit = {}): Promise<any> {
-		this.refreshToken();
-		const token = cookies.get('AccessToken');
-		return fetch(url, {
-			method: 'POST',
-			headers: {
-				...headers,
-				'Authorization': `Bearer ${token}`
-			},
-			body
-		})
-			.then(response => {
-				const passingStatuses = [200, 201];
-
-				if (!passingStatuses.includes(response.status)) {
-					throw `${response.status} ${response.statusText}`;
-				}
-
-				return response.json()
+		return this.refreshToken().then(() => {
+			const token = cookies.get('AccessToken');
+			return fetch(url, {
+				method: 'POST',
+				headers: {
+					...headers,
+					'Authorization': `Bearer ${token}`
+				},
+				body
 			})
+				.then(response => {
+					const passingStatuses = [200, 201];
+
+					if (!passingStatuses.includes(response.status)) {
+						throw `${response.status} ${response.statusText}`;
+					}
+
+					return response.json();
+				});
+		});
 	}
 
-	static refreshToken (): Promise<any> | void {
+	static refreshToken (): Promise<any> {
 		const token = cookies.get('AccessToken');
 		const decoded_token: any = token ? decode(token) : null;
 		if(decoded_token && decoded_token.exp - Math.round(Date.now() / 1000) < 10) {
@@ -56,6 +59,9 @@ export default class RequestService {
 					'Authorization': `Bearer ${token}`
 				},
 			})
+		}
+		else {
+			return Promise.resolve();
 		}
 	}
 }
